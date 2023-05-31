5d4d32715a3ceba82715643588d2f714bf1c5309
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-31 10:49:39 -0300

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "docs"),
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
