cc9e15964a10ff00ac5367fb5077ea7bc9aae820
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-31 19:30:49 -0300

    html/styles/assets home section and header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import fotoLucas from './assets/recursos/foto-lucas.png';
+import iconoAzul from './assets/logos/logo-azul.svg';
+import iconoBlanco from './assets/logos/logo-blanco.svg';
+
+const logoAzul = document.getElementById('icono');
+const logoBlanco = document.getElementById('home-white-logo')
+const homeLucas = document.getElementById('imagen-lucas');
+homeLucas.src = fotoLucas;
+logoAzul.src = iconoAzul;
+logoBlanco.src = iconoBlanco;
