0b6abebc1ae16d25556a21362d3d512139dd1480
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-31 20:23:56 -0300

    Final styles for home and nav

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import fotoLucas from './assets/recursos/foto-lucas.png';
 import iconoAzul from './assets/logos/logo-azul.svg';
 import iconoBlanco from './assets/logos/logo-blanco.svg';
 
+const header = document.getElementById('header')
+
 const logoAzul = document.getElementById('icono');
 const logoBlanco = document.getElementById('home-white-logo')
 const homeLucas = document.getElementById('imagen-lucas');
 homeLucas.src = fotoLucas;
 logoAzul.src = iconoAzul;
 logoBlanco.src = iconoBlanco;
+
+window.addEventListener ('scroll', ()=> {
+    let show = false;
+    if (window.scrollY >= 150 && !show){
+        header.classList.add('show-header');
+        show = true;
+    } else if (window.scrollY < 150) {
+        header.classList.remove('show-header');
+        show = false;
+    }
+    
+})
