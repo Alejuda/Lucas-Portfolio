df64a3dc8e6dcd47ee845bd795fc2a05de64ec51
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-31 12:29:39 -0300

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
   output: {
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, 'docs'),
   },
-  mode: "development",
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
   module: {
