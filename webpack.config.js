e6c1de5d9dcee97dd5ef56850724989d3a239fe0
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-31 11:52:32 -0300

    Finish webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,16 @@ module.exports = {
       template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
