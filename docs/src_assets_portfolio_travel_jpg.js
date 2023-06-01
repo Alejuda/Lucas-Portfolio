bc9084310f91e3bae209e2b23ca8795dc7c1ab00
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-01 11:01:09 -0300

    Add portfolio images and some styles.

---
+++ docs/src_assets_portfolio_travel_jpg.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunklucas_portfolio"] = self["webpackChunklucas_portfolio"] || []).push([["src_assets_portfolio_travel_jpg"],{
+
+/***/ "./src/assets/portfolio/travel.jpg":
+/*!*****************************************!*\
+  !*** ./src/assets/portfolio/travel.jpg ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"8e8e0973d70c82bf442b.jpg\";\n\n//# sourceURL=webpack://lucas-portfolio/./src/assets/portfolio/travel.jpg?");
+
+/***/ })
+
+}]);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,86 @@
-import './style.css';
-import fotoLucas from './assets/recursos/foto-lucas.png';
-import iconoAzul from './assets/logos/logo-azul.svg';
-import iconoBlanco from './assets/logos/logo-blanco.svg';
+import "./style.css";
+import fotoLucas from "./assets/recursos/foto-lucas.png";
+import iconoAzul from "./assets/logos/logo-azul.svg";
+import iconoBlanco from "./assets/logos/logo-blanco.svg";
+import { getPhotoPath } from './photoImporter.js';
 
-const header = document.getElementById('header')
+const header = document.getElementById("header");
+const portfolioSection = document.getElementById("portfolio");
+const works = [
+  {
+    name: "YSY-A",
+    photo: "./assets/portfolio/ysy-a.jpg",
+  },
+  {
+    name: "Martha",
+    photo: "./assets/portfolio/martha.jpg",
+  },
+  {
+    name: "Valkiria",
+    photo: "./assets/portfolio/valkiria.jpg",
+  },
+  {
+    name: "Outdoor",
+    photo: "./assets/portfolio/outdoor.jpg",
+  },
+  {
+    name: "Travel",
+    photo: "./assets/portfolio/travel.jpg",
+  },
+  {
+    name: "Polo",
+    photo: "./assets/portfolio/polo.jpg",
+  },
+  {
+    name: "Street",
+    photo: "./assets/portfolio/street.jpg",
+  },
+  {
+    name: "Artash",
+    photo: "./assets/portfolio/artash.jpg",
+  },
+];
 
-const logoAzul = document.getElementById('icono');
-const logoBlanco = document.getElementById('home-white-logo')
-const homeLucas = document.getElementById('imagen-lucas');
+const logoAzul = document.getElementById("icono");
+const logoBlanco = document.getElementById("home-white-logo");
+const homeLucas = document.getElementById("imagen-lucas");
 homeLucas.src = fotoLucas;
 logoAzul.src = iconoAzul;
 logoBlanco.src = iconoBlanco;
 
-window.addEventListener ('scroll', ()=> {
-    let show = false;
-    if (window.scrollY >= 150 && !show){
-        header.classList.add('show-header');
-        show = true;
-    } else if (window.scrollY < 150) {
-        header.classList.remove('show-header');
-        show = false;
-    }
-    
-})
+window.addEventListener("scroll", () => {
+  let show = false;
+  if (window.scrollY >= 150 && !show) {
+    header.classList.add("show-header");
+    show = true;
+  } else if (window.scrollY < 150) {
+    header.classList.remove("show-header");
+    show = false;
+  }
+});
+
+const renderWorks = () => {
+  let portfolioInner = '';
+  works.forEach((work) => {
+    const img = document.createElement("img");
+    img.src = portfolioInner += `
+    <div class="work-container">
+        <img class="work-photo">
+        <h3 class="work-name">${work.name}</h3>
+    </div>
+  `;
+  });
+  portfolioSection.innerHTML = portfolioInner;
+  const worksArray = Array.from(document.querySelectorAll(".work-photo"));
+  worksArray.forEach((work, idx) => {
+    getPhotoPath(works[idx].photo)
+      .then(photoPath => {
+        work.src = photoPath
+      })
+      .catch(error => {
+        console.error('error loading photo: ' + error)
+      })
+  });
+};
+
+renderWorks();
