d5ab20878dc7ea825c4ccbcbb2d5fbe19668f4f4
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-01 13:03:31 -0300

    Add portfolio section/ styles, assets, and fix linters

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-import "./style.css";
-import fotoLucas from "./assets/recursos/foto-lucas.png";
-import iconoAzul from "./assets/logos/logo-azul.svg";
-import iconoBlanco from "./assets/logos/logo-blanco.svg";
+import './style.css';
+import fotoLucas from './assets/recursos/foto-lucas.png';
+import iconoAzul from './assets/logos/logo-azul.svg';
+import iconoBlanco from './assets/logos/logo-blanco.svg';
 import { getPhotoPath } from './photoImporter.js';
 
-const header = document.getElementById("header");
-const portfolioSection = document.getElementById("portfolio");
+const header = document.getElementById('header');
+const portfolioSection = document.getElementById('portfolio');
 const works = [
   {
-    name: "YSY-A",
-    photo: "./assets/portfolio/ysy-a.jpg",
+    name: 'YSY-A',
+    photo: './assets/portfolio/ysy-a.jpg',
   },
   {
-    name: "Martha",
-    photo: "./assets/portfolio/martha.jpg",
+    name: 'Martha',
+    photo: './assets/portfolio/martha.jpg',
   },
   {
-    name: "Valkiria",
-    photo: "./assets/portfolio/valkiria.jpg",
+    name: 'Valkiria',
+    photo: './assets/portfolio/valkiria.jpg',
   },
   {
-    name: "Outdoor",
-    photo: "./assets/portfolio/outdoor.jpg",
+    name: 'Outdoor',
+    photo: './assets/portfolio/outdoor.jpg',
   },
   {
-    name: "Travel",
-    photo: "./assets/portfolio/travel.jpg",
+    name: 'Travel',
+    photo: './assets/portfolio/travel.jpg',
   },
   {
-    name: "Polo",
-    photo: "./assets/portfolio/polo.jpg",
+    name: 'Polo',
+    photo: './assets/portfolio/polo.jpg',
   },
   {
-    name: "Street",
-    photo: "./assets/portfolio/street.jpg",
+    name: 'Street',
+    photo: './assets/portfolio/street.jpg',
   },
   {
-    name: "Artash",
-    photo: "./assets/portfolio/artash.jpg",
+    name: 'Artash',
+    photo: './assets/portfolio/artash.jpg',
   },
 ];
 
-const logoAzul = document.getElementById("icono");
-const logoBlanco = document.getElementById("home-white-logo");
-const homeLucas = document.getElementById("imagen-lucas");
+const logoAzul = document.getElementById('icono');
+const logoBlanco = document.getElementById('home-white-logo');
+const homeLucas = document.getElementById('imagen-lucas');
 homeLucas.src = fotoLucas;
 logoAzul.src = iconoAzul;
 logoBlanco.src = iconoBlanco;
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   let show = false;
   if (window.scrollY >= 150 && !show) {
-    header.classList.add("show-header");
+    header.classList.add('show-header');
     show = true;
   } else if (window.scrollY < 150) {
-    header.classList.remove("show-header");
+    header.classList.remove('show-header');
     show = false;
   }
 });
@@ -62,24 +62,22 @@ window.addEventListener("scroll", () => {
 const renderWorks = () => {
   let portfolioInner = '';
   works.forEach((work) => {
-    const img = document.createElement("img");
-    img.src = portfolioInner += `
+    portfolioInner += `
+    <a href="#${work.name}">
     <div class="work-container">
         <img class="work-photo">
         <h3 class="work-name">${work.name}</h3>
     </div>
+    </a>
   `;
   });
   portfolioSection.innerHTML = portfolioInner;
-  const worksArray = Array.from(document.querySelectorAll(".work-photo"));
+  const worksArray = Array.from(document.querySelectorAll('.work-photo'));
   worksArray.forEach((work, idx) => {
     getPhotoPath(works[idx].photo)
-      .then(photoPath => {
-        work.src = photoPath
-      })
-      .catch(error => {
-        console.error('error loading photo: ' + error)
-      })
+      .then((photoPath) => {
+        work.src = photoPath;
+      });
   });
 };
 
